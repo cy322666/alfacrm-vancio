b9a48e952031102c43287de313a7c7e40c6f1200
cy322666 <cy322666@gmail.com>
2023-07-09 11:14:20 +0200

    add log and hotfix

--- application/app/Http/Controllers/Controller.php
+++ application/app/Http/Controllers/Controller.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
@@ -22,8 +23,14 @@ class Controller extends BaseController
      */
     public function __construct()
     {
-        $account = Account::query()->first();
+        try {
+            $account = Account::query()->first();
 
-        $this->amoApi = (new Client($account))->init();
+            $this->amoApi = (new Client($account))->init();
+
+        } catch (\Throwable $e) {
+
+            Log::error(__METHOD__.' : '.$e->getMessage().' '.$e->getFile().' '.$e->getLine());
+        }
     }
 }
